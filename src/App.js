493fcaf72f4a88cfb58bff65bd50d97d97fc7d62
Stephen-M5321 <127414937+Stephen-M5321@users.noreply.github.com>
2023-04-27 22:45:14 +0100

    connected with backend Auth and hash

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import React from "react";
 import Login from './components/Login'
 import Register from './components/Register'
 
-import {useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import { getCookie } from "./common"
+
+import { authCheck } from "./utils"
 
 const App = () => {
 
   const [user, setUser] = useState()
+
+  useEffect(()=> {
+    let jwt = getCookie("jwt_token")
+    console.log("!!!!!")
+    console.log(jwt)
+    if (jwt !== false){
+      loginWithToken(jwt)
+    }
+
+  }, [])
+
+  const loginWithToken = async (jwt) => {
+    const user = await authCheck(jwt)
+    setUser(user)
+  }
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,7 @@
 // Login user
 
+import { writeCookie } from "../common"
+
 export const loginUser = async (username, email, password, newUser) => {
     try {
         const response = await fetch("http://localhost:5002/users/login", {
@@ -16,6 +18,9 @@ export const loginUser = async (username, email, password, newUser) => {
         const data = await response.json()
         console.log(data)
         newUser(data.user.username)
+
+        writeCookie("jwt_token", data.user.token, 7)
+
     } catch (error) {
         console.log(error)
     }
@@ -40,4 +45,24 @@ export const registerUser = async (username, email, password) => {
         console.log(error)
     }
 }
+
+export const authCheck = async (jwtToken) => {
+    try {
+       const response = await fetch("http://localhost:5002/users/authCheck", {
+        method: "GET",
+        headers: {
+            "Content-Type" : "application/json",
+            "Authorization": `Bearer $(jwtToken)`
+        }
+       }) 
+       const data = await response.json()
+       console.log(data)
+       return data.user.username
+
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
 // TODO: add register fetch function here
